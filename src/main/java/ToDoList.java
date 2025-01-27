9cd90716bb0bb485996d5abc0eee59df07389f56
mohammedhamdhan <mohammedhamdhan09@gmail.com>
2025-01-27 23:40:39 +0800

    Add new ToDoList class
    Add functionality to store entries and recite them when asked

---
+++ src/main/java/ToDoList.java
@@ -0,0 +1,10 @@
+public class ToDoList {
+    private String entry = "";
+
+    public String getEntry() {
+        return entry;
+    }
+    public void setEntry(String entry) {
+        this.entry = entry;
+    }
+}
